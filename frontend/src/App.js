72881eae6921894c3d7e8a269b38458368a55374
priyanshu333 <priyanshughosh333@gmail.com>
2025-06-05 20:56:56 +0530

    added a marker without icon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer } from 'react-leaflet'
-
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import L from 'leaflet';
 
 function App() {
   return (
@@ -12,6 +12,11 @@ function App() {
       attribution= '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
+      <Marker
+        position={[48.8566,2.3522]}>
+        <Popup>A testing marker</Popup>
+      </Marker>
+
 
     </MapContainer>
     
