ef60714871003d8463847a979d5f73374333a3a4
priyanshu333 <priyanshughosh333@gmail.com>
2025-06-05 20:35:18 +0530

    added leaflet in reactapp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer } from 'react-leaflet'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MapContainer center={[48.8566,2.3522]} zoom={13}>
+
+      <TileLayer
+      attribution= '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+      />
+
+    </MapContainer>
+    
   );
 }
 
